fb94bf329515e4c22b841c70c71e338db82f3fe2
cassanof <federico.cassano@federico.codes>
2023-06-20 13:39:26 -0700

    changed deps, prepublish

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,5 @@ pub mod errors;
 pub mod visitor;
 
 lazy_static! {
-    static ref LANG: tree_sitter::Language = tree_sitter_luau::language();
+    static ref LANG: tree_sitter::Language = luau_ast_rs_grammar::language();
 }
